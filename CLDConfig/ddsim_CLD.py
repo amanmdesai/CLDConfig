33e33fbc6ae24505494ca5cc009f420d83049c61
amanmdesai <amanmukeshdesai@gmail.com>
2026-09-21 12:53:15 +0930

    Rename FLARE variants to k4run_cld_reco.py and ddsim_CLD.py

--- CLDConfig/ddsim_CLD.py
+++ CLDConfig/ddsim_CLD.py
@@ -56,7 +56,6 @@ SIM.enableGun = False
 SIM.inputFiles = []
 ## Macro file to execute for runType 'run' or 'vis'
 SIM.macroFile = ""
-## number of events to simulate, used in batch mode
 ## Outputfile from the simulation
 SIM.outputFile = "CLD_SIM.edm4hep.root"
 ## Verbosity use integers from 1(most) to 7(least) verbose
